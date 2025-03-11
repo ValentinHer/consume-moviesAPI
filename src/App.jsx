778474a11d9cc634da451fc469bb76d8c096104a
ValentinHer <valentinhernandezhernandez18@gmail.com>
2025-03-11 17:12:29 -0600

    [add] buscador de peliculas añadido en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import "./styles/app.css";
+import {} from "bootstrap";
 
 function App() {
+  const [valueBuscador, setValueBuscador] = useState("");
+
+  const handleChange = (event) => {
+    setValueBuscador(event.target.value);
+    console.log(event.target.value);
+  }
+
   return (
     <>
-    <div className="mainContainer container-fluid bg-primary d-flex justify-content-center align-items-center" >
-      <h1>
-        Home
-      </h1>
-    </div>
+      <div className="mainContainer bg-primary p-5 container-fluid d-flex justify-content-center align-items-center flex-column ">
+        <div class="mb-3 w-50 ">
+          <input
+            type="email"
+            class="form-control form-control-lg"
+            id="exampleFormControlInput1"
+            placeholder="Buscar Película"
+            value={valueBuscador}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
