22e55ee549eb957db7281f4cc8930576402f53e5
ValentinHer <valentinhernandezhernandez18@gmail.com>
2025-03-19 13:55:18 -0600

    [add] hooks para buscar peliculas terminado

---
+++ src/hooks/movie-genres.js
@@ -0,0 +1,19 @@
+import axios from "axios"
+import { publicAccessKey } from "./constants"
+
+export const getGenres = async() => {
+    const options = {
+        method: "GET",
+        headers: {
+            accept: "application/json",
+            Authorization: `Bearer ${publicAccessKey}`
+        }
+    }
+
+    try {
+        const response = await axios.get("https://api.themoviedb.org/3/genre/movie/list?language=es", options);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
